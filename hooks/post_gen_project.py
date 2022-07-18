1de82ff88dfa4768ed1580b5e44dbec074005420
KyoHattori <kyo.hattori@abejainc.com>
2022-07-19 00:20:40 +0900

    fix post hooks reline

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,9 +2,9 @@
 
 is_kaggle_competition_str = '{{ cookiecutter.is_kaggle}}'
 
-if is_kaggle_competition_str == "Yes":
-    dl_command_str = "#!/bin/bash"
-    dl_command_str += "kaggle competitions download -c '{{ cookiecutter.competition_name }}'"
-    dl_command_str += "unzip -q input/{{ cookiecutter.competition_name }}.zip -d input/{{ cookiecutter.competition_name }}"
+if is_kaggle_competition_str.lower() == "yes":
+    dl_command_str = "#!/bin/bash\n"
+    dl_command_str += "kaggle competitions download -c '{{ cookiecutter.competition_name }}'\n"
+    dl_command_str += "unzip -q input/{{ cookiecutter.competition_name }}.zip -d input/{{ cookiecutter.competition_name }}\n"
     with open("script/download_data.sh", "w") as f:
         f.write(dl_command_str)
