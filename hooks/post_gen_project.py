13acd7da4b7880076287224bcbd1a114a5e478d5
KyoHattori <kyo.hattori@abejainc.com>
2022-07-19 00:26:32 +0900

    fix posthook dir

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,12 @@
-
+from pathlib import Path
 
 is_kaggle_competition_str = '{{ cookiecutter.is_kaggle}}'
 
 if is_kaggle_competition_str.lower() == "yes":
+    Path("input").mkdir(exist_ok=True)
     dl_command_str = "#!/bin/bash\n"
     dl_command_str += "kaggle competitions download -c '{{ cookiecutter.competition_name }}'\n"
-    dl_command_str += "unzip -q input/{{ cookiecutter.competition_name }}.zip -d input/{{ cookiecutter.competition_name }}\n"
+    dl_command_str += "unzip -q {{ cookiecutter.competition_name }}.zip -d input/{{ cookiecutter.competition_name }}\n"
+    dl_command_str += "unzip -q {{ cookiecutter.competition_name }}.zip"
     with open("script/download_data.sh", "w") as f:
         f.write(dl_command_str)
