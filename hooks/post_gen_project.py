6f8a325fb0e9b3a807543e23cbd060d3678e6d53
KyoHattori <kyo.hattori@abejainc.com>
2022-07-19 00:29:30 +0900

    fix posthook unzip to rm

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,6 +7,6 @@ if is_kaggle_competition_str.lower() == "yes":
     dl_command_str = "#!/bin/bash\n"
     dl_command_str += "kaggle competitions download -c '{{ cookiecutter.competition_name }}'\n"
     dl_command_str += "unzip -q {{ cookiecutter.competition_name }}.zip -d input/{{ cookiecutter.competition_name }}\n"
-    dl_command_str += "unzip -q {{ cookiecutter.competition_name }}.zip"
+    dl_command_str += "rm {{ cookiecutter.competition_name }}.zip"
     with open("script/download_data.sh", "w") as f:
         f.write(dl_command_str)
